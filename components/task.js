fcb663fc4a57f8a15198926118bab0f567e76dcb
Julien Roche <merlin441977@gmail.com>
2023-11-22 21:53:52 +0100

    finition de l'apparence des lis

--- components/task.js
+++ components/task.js
@@ -7,8 +7,23 @@ export function createLi(list){
     
     for(let el of list){
     const li=document.createElement('li')
+    li.classList.add("todo", "list-group-item", "d-flex", "align-items-center")
+    const checkBox=document.createElement('input')
+    checkBox.type='checkbox'
+    checkBox.classList.add("form-check-input")
+    const labelTitle=document.createElement('label')
+    labelTitle.classList.add("ms-2", "form-check-label")
+    const labelBin=document.createElement('label')
+    labelBin.classList.add("ms-auto","btn","btn-danger", "btn-sm")
+    const bin=document.createElement('i')
+    bin.classList.add("bi-trash")
+   
+    li.append(checkBox)
+    li.append(labelTitle)
+    li.append(labelBin)
+    labelBin.append(bin)
     ul.append(li)
-    li.innerHTML=el.title
+    labelTitle.innerHTML=el.title
     }
 
 }
